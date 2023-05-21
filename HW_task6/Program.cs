4541c791ec26171e664db3b457044dee05306b79
Dzihiko <anna.bond1702@gmail.com>
2023-05-21 21:58:45 +0300

    Четность числа

---
+++ HW_task6/Program.cs
@@ -0,0 +1,12 @@
+﻿// четность числа
+Console.WriteLine("Text a number");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
