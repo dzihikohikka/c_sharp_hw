c0c911fe1fbe729b06b9bbabaae8d53a5dafff62
Dzihiko <anna.bond1702@gmail.com>
2023-05-21 20:28:40 +0300

    approuve of squr

---
+++ task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// принимает 2 числа, проверка на первое квадрат второго
+
+Console.WriteLine("text first number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("text second number");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b*b)
+{
+    Console.WriteLine("Yes, " + a + " qwuare of " + b);
+}
+else 
+{
+    Console.WriteLine("No");
+}
