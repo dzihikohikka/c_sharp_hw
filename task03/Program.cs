93fd6da849ed2dea4a4064ea21e0e8a0ba709111
Dzihiko <anna.bond1702@gmail.com>
2023-05-21 21:12:56 +0300

    диапазон

---
+++ task03/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывод диапащона всех целых чисел до этого
+Console.WriteLine("Text a number");
+int a = int.Parse(Console.ReadLine());
+int b = a * -1;
+int temp;
+
+if (a < 0)
+{
+    temp = a;
+    a = b;
+    b = temp;
+}
+while (b <= a)
+{
+    Console.Write(b + " ");
+    b += 1;
+}
