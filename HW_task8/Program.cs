4168ed58df4f7a9749c535513f8983388830528b
Dzihiko <anna.bond1702@gmail.com>
2023-05-21 22:10:19 +0300

    все четные до числа

---
+++ HW_task8/Program.cs
@@ -0,0 +1,10 @@
+﻿// все четные до него
+Console.WriteLine("Text a number");
+int a = int.Parse(Console.ReadLine());
+int b = 2;
+
+while (b <= a)
+{
+    Console.Write(b + ", ");
+    b = b + 2;
+}
