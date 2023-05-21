ce515054abf387124a777321565f65a187488065
Dzihiko <anna.bond1702@gmail.com>
2023-05-21 20:48:51 +0300

    days

---
+++ task02/Program.cs
@@ -0,0 +1,32 @@
+﻿// номер дня недели - день недели
+Console.Write("Write day number: ");
+int x = int.Parse(Console.ReadLine());
+
+if (x == 1)
+{
+    Console.WriteLine("Monday");
+}
+if (x == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if (x == 3)
+{
+    Console.WriteLine("Wenesday");
+}
+if (x == 4)
+{
+    Console.WriteLine("Thursday");
+}
+if (x == 5)
+{
+    Console.WriteLine("Friday");
+}
+if (x == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if (x == 7)
+{
+    Console.WriteLine("Sunday");
+}
