6a643665d1572f26b330b28719952c8d70c9dfcf
Dzihiko <anna.bond1702@gmail.com>
2023-05-21 21:43:19 +0300

    Max and min

---
+++ HW_task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// из двух чисел макс и мин
+Console.WriteLine("Text first number ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Text second number ");
+int b = int.Parse(Console.ReadLine());
+
+if ( a > b)
+{
+    Console.WriteLine("Max:" + a + " min:" + b);
+}
+else
+{
+    Console.WriteLine("Max:" + b + " min:" + a);
+}
