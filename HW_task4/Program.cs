7bd042657f34a992bcacc63d3cae9010978e63be
Dzihiko <anna.bond1702@gmail.com>
2023-05-21 21:54:15 +0300

    максимальное из трех

---
+++ HW_task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// масимальное из трех чисел
+Console.WriteLine("Text three numbers");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if ( max < b)
+{
+    max = b;
+}
+else if ( max < c)
+{
+    max = c;
+}
+if (max < c)
+{
+    max = c;
+    Console.WriteLine("max: " + max);
+}
+else
+{
+    Console.WriteLine("max: " + max);
+}
