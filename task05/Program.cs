ee9e1f9a3b2cef5186ee120c94a970bf6b0f9c59
Dzihiko <anna.bond1702@gmail.com>
2023-06-01 01:12:14 +0300

    макс из двузнач рандом

---
+++ task05/Program.cs
@@ -0,0 +1,17 @@
+﻿// программа выводит случайное число из отрезка 0-99
+// и показывает наибольшую цифру числа
+
+int num = new Random().Next(10, 100);
+int a = num/10;
+int b = num%10;
+
+Console.WriteLine("Random number: " + num);
+
+if (a > b)
+{
+    Console.WriteLine("max is: " + a);
+}
+else 
+{
+    Console.WriteLine("max is: " + b);
+}
