2407c02696a513e77380940a0f20f294ce826a7f
Dzihiko <anna.bond1702@gmail.com>
2023-06-01 04:33:02 +0300

    четверти х у

---
+++ task08/Program.cs
@@ -0,0 +1,32 @@
+﻿// определить четверть точки по х у 
+
+int a = Prompt("text x: ");
+int b = Prompt("text y: ");
+FindCoordinate(a, b);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+void FindCoordinate(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("first");
+    }
+    if (x < 0 && y > 0)
+    {
+        Console.WriteLine("second");
+    }
+        if (x < 0 && y < 0)
+    {
+        Console.WriteLine("third");
+    }
+        if (x > 0 && y < 0)
+    {
+        Console.WriteLine("forth");
+    }
+}
