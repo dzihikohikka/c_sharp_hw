2434ce276c5f2778d47c19603621d1696083dd09
Dzihiko <anna.bond1702@gmail.com>
2023-06-01 19:50:26 +0300

    расстояние х у

---
+++ task09/Program.cs
@@ -0,0 +1,21 @@
+﻿// найти расстояние между точками
+int Prompt (string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+int x1 = Prompt ("text x1: ");
+int y1 = Prompt ("text y1: ");
+
+int x2 = Prompt ("text x2: ");
+int y2 = Prompt ("text y2: ");
+
+void CalcDistance (int a, int b, int c, int d)
+{
+    double s = Math.Sqrt((a-b)*(a-b)+(c-d)*(c-d));
+    Console.WriteLine("Distance is: "+s);
+}
+CalcDistance(x1, x2, y1, y2);
+
