5a546b15b949b289f47773f21d6d68c4596a634f
Dzihiko <anna.bond1702@gmail.com>
2023-06-01 20:08:45 +0300

    квадраты до числа

---
+++ task10/Program.cs
@@ -0,0 +1,22 @@
+﻿// квадраты до числа n
+
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void SqPrint (int a)
+{
+    int i = 0;
+    while (i < a)
+    {
+      i = i+1;
+      Console.Write(i*i+" ");
+    }
+}
+
+int num = Prompt ("text number: ");
+SqPrint(num);
