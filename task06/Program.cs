fe4db7819f2253dd07ef9e273ebceead4ae1a89d
Dzihiko <anna.bond1702@gmail.com>
2023-06-01 03:03:48 +0300

    удаление десятков

---
+++ task06/Program.cs
@@ -0,0 +1,17 @@
+﻿// из рандомного трехзнач удалить вторую цифру 578 - 58
+
+int FindFirst(int num)
+{
+    int firstNum = num / 100;
+    return firstNum;
+}
+
+int FindLast(int num)
+{
+    int lastNum = num % 10;
+    return lastNum;
+}
+
+int number = new Random().Next(99, 1000);
+Console.WriteLine("number is: " + number);
+Console.WriteLine(FindFirst(number) + "" + FindLast(number));
