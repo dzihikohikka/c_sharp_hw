9adb3483fe973ddf468a96a385722a8d87ce5b43
Dzihiko <anna.bond1702@gmail.com>
2023-06-03 00:13:54 +0300

    кубы

---
+++ HW_task23/Program.cs
@@ -0,0 +1,21 @@
+﻿// кубы до n
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void CubePrint (int a)
+{
+    int i = 0;
+    while (i < a)
+    {
+      i = i+1;
+      Console.Write(i*i*i+" ");
+    }
+}
+
+int num = Prompt ("text number: ");
+CubePrint(num);
