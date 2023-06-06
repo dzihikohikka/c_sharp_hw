80ec670f4ca19967606564954306313b8fc23ac3
Dzihiko <anna.bond1702@gmail.com>
2023-06-06 16:02:41 +0300

    массив 01

---
+++ task14/Program.cs
@@ -0,0 +1,11 @@
+﻿// вывод массива из 8 эл-тов заполненный 0 и 1 ( 1 1 0 0 1 0 1 0)
+
+int [] array = new int [8];
+
+for (int i = 0; i < 9; i++)
+{
+    array[i] = new Random().Next(0,2);
+    Console.Write(array[i]+", ");
+}
+
+
