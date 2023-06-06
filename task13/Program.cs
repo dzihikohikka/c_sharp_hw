699c41ca17025e9bf6bb8966102937a3373deb28
Dzihiko <anna.bond1702@gmail.com>
2023-06-06 15:38:40 +0300

    факториал(произв до числа)

---
+++ task13/Program.cs
@@ -0,0 +1,22 @@
+﻿// принимает число - вывод произведение чисел до него
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+void Compos(int num)
+{
+    int C = 1;
+
+    for (int i = 1; i <= num; i++)
+    {
+        C = C * i;
+    }
+    Console.WriteLine("composition = " + C);
+}
+
+int number = Prompt("text number");
+Compos(number);
