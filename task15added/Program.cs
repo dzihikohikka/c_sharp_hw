d1f07a610c1d06d585f053f5b870be35e6dabeb7
Dzihiko <anna.bond1702@gmail.com>
2023-06-06 16:46:58 +0300

    FizzBuzz

---
+++ task15added/Program.cs
@@ -0,0 +1,31 @@
+﻿// вывод чисел от 1 до 100, где все числа кратные 3 
+// заменены на Fizz, кратные 5 Buzz 
+// если и 3 и 5 то FizzBuzz
+
+
+int [] array = new int[101];
+
+for (int i = 1; i < 101; i++)
+{
+    array [i] = i;
+    if (array [i]  % 15 == 0)
+    {
+        Console.Write("FizzBuzz, ");
+    }
+    else if (array [i] % 3 == 0)
+    {
+        Console.Write("Fizz, ");
+    }
+    else if (array [i] % 5 == 0)
+    {
+        Console.Write("Buzz, ");
+    }
+    else
+    {
+    Console.Write(array[i]+ ", ");
+    }
+}
+
+
+
+
