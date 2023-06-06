ee53216d0c8009cb6f29674a17ca2f936ea119fe
Dzihiko <anna.bond1702@gmail.com>
2023-06-06 15:33:32 +0300

    цифры числа

---
+++ task12/Program.cs
@@ -0,0 +1,22 @@
+﻿// на вход число - вывод кол-во цифр
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+void Digits(int b)
+{
+    int count = 0;
+    while (b > 0)
+    {
+       b = b / 10;
+       count++;
+    }
+    Console.WriteLine("digits is " + count);
+}
+
+int number = Prompt("text a number ");
+Digits(number);
