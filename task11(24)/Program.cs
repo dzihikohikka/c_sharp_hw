aa954b2617c9f38246e0a63418fcab8f3d681512
Dzihiko <anna.bond1702@gmail.com>
2023-06-06 15:17:28 +0300

    сумма до числа

---
+++ task11(24)/Program.cs
@@ -0,0 +1,22 @@
+﻿// принимаем число а - выдаем сумму чисел до а
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+void Sum(int num)
+{
+    int S = 0;
+
+    for (int i = 1; i <= num; i++)
+    {
+        S = S + i;
+    }
+    Console.WriteLine("sum = " + S);
+}
+
+int number = Prompt("text number");
+Sum(number);
