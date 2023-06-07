0fc70512c374c1139eb441b300fb7f45a0ffa1f2
Dzihiko <anna.bond1702@gmail.com>
2023-06-07 17:42:37 +0300

    сумма цифр в числе

---
+++ HW_task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// сумма цифр в числе
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void DigitSum (int a)
+{
+    int sum = 0;
+    int ost = 0;
+
+    while (a > 0)
+    {
+        ost = a % 10; 
+        a = a / 10;
+        sum = sum + ost;
+    }
+    Console.WriteLine("summ of gigits: "+sum);
+}
+
+int number = Prompt("text a number");
+DigitSum(number);
