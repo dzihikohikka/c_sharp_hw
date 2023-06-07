77ae507604cecd82eb45e9cc144813a65d658440
Dzihiko <anna.bond1702@gmail.com>
2023-06-07 17:15:27 +0300

    а в степень б

---
+++ HW_task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// а в степень б
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void Step (int x, int y)
+{
+    int result = 1;
+    for (int i = 1; i <= y; i++)
+    {
+        result = result*x;
+    }
+    Console.Write(result);
+}
+
+int a = Prompt("text number a");
+int b = Prompt("text step b");
+Step(a, b);
