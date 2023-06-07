5469f5c30468b6ce2e12de4db881009e50ea7128
Dzihiko <anna.bond1702@gmail.com>
2023-06-07 17:50:02 +0300

    массив из чисел

---
+++ HW_task29/Program.cs
@@ -0,0 +1,22 @@
+﻿// массив из 8 чисел
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+
+void PrintArr (int a)
+{
+    int [] array = new int [a];
+
+    for (int i = 0; i <= a; i++)
+    {
+    array[i] = new Random().Next(0,100);
+    Console.Write(array[i]+", ");
+    }
+}
+
+int len = Prompt("text a letgh");
+PrintArr(len);
