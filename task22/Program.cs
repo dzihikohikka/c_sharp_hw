6fd3be66120fddb6b6671d5f66abdaa55db79d60
Dzihiko <anna.bond1702@gmail.com>
2023-06-14 23:02:14 +0300

    двоичное число

---
+++ task22/Program.cs
@@ -0,0 +1,37 @@
+﻿// перевод числа в двоичную систему
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+void ZeroOne(int num)
+{
+    int ost = -1;
+    int count = 0;
+    int num1 = num;
+    while (num1 > 0)
+    {
+        num1 = num1 /2;
+        count = count + 1;
+    }
+
+    int [] arr = new int [count];
+    for (int i = 0; i < count; i++)
+    {
+        ost = num % 2;
+        num = num / 2;
+        arr [i] = ost;
+        Console.Write(arr[i]);
+    }
+    Console.WriteLine();
+    for (int i = count -1; i >= 0; i--)
+    {
+        Console.Write(arr[i]);
+    }
+    }
+
+int number = Prompt("text number");
+ZeroOne(number);
