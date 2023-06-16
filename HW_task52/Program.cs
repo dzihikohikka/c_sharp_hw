e243d08e34ad0a69a4e00f80d72df6ad91efd7e3
Dzihiko <anna.bond1702@gmail.com>
2023-06-16 18:35:50 +0300

    среднее в столбце

---
+++ HW_task52/Program.cs
@@ -0,0 +1,53 @@
+﻿// cреднее арифметич каждого СТОЛБЦА
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+
+void PrintArray(int [,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        Console.Write($"{matr[i,j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+void FillArray(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 11);
+        }
+    }
+}
+
+void ArifSredStolb (int [,] matr)
+{
+    int [] sum = new int [matr.GetLength(0)];
+    double Sred = 0;
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            sum [j] = matr[i,j]+ sum[j];
+        }
+        Console.WriteLine(sum[j]/matr.GetLength(0)); 
+    }   
+}
+
+int m = Prompt("text m:");
+int n = Prompt("text n:");
+int [,] matrix = new int [m,n];
+FillArray(matrix);
+PrintArray(matrix);
+ArifSredStolb(matrix);
