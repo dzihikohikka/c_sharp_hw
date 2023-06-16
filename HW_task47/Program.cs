9be81970ce473f67922a078745ce175bcaecda22
Dzihiko <anna.bond1702@gmail.com>
2023-06-16 17:29:46 +0300

    заполнение m n массива

---
+++ HW_task47/Program.cs
@@ -0,0 +1,38 @@
+﻿// массив m x n заполненный случайными веществ числами
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+
+void PrintArray(int [,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        Console.Write($"{matr[i,j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+void FillArray(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(-10, 11);
+        }
+    }
+}
+
+int m = Prompt("text m:");
+int n = Prompt("text n:");
+int [,] matrix = new int [m,n];
+FillArray(matrix);
+PrintArray(matrix);
