b62b1e4da736389294b273aa4d3d03ea2f4e6560
Dzihiko <anna.bond1702@gmail.com>
2023-06-18 00:19:28 +0300

    вывод m+n

---
+++ task23/Program.cs
@@ -0,0 +1,41 @@
+﻿// записать массив где число задается по формуле i+j 
+// 0 1 2 3 
+// 1 2 3 4
+// 2 3 4 5
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+void PrintArray(int [,] matr)
+{
+    for (int i = 0; i<matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0);i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = i+j;
+        }
+    }
+}
+
+int m = Prompt("text m");
+int n = Prompt("text n");
+int [,] matrix = new int [m,n];
+
+FillArray(matrix);
+PrintArray(matrix);
