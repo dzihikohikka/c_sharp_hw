ea0b6b9176c9d72d2afecbb483b059904f1b4517
Dzihiko <anna.bond1702@gmail.com>
2023-06-18 04:19:08 +0300

    частота встречания в массиве

---
+++ task27/Program.cs
@@ -0,0 +1,58 @@
+﻿// частота встречания из введенных данных
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+int [] GetFillFromKeyBoard (int len)
+{
+     int [] arr = new int [len];
+
+     for (int i = 0; i < len; i++)
+     {
+        Console.Write("text element ");
+        arr [i] = int.Parse(Console.ReadLine()!);
+     }
+     return arr;
+}
+
+void PrintArray(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+void Often (int [] arr)
+{
+    for ( int i = 0; i < arr.Length; i++)
+    {
+        int count = 1;
+        int check = arr[i];
+        for (int j = i+1; j < arr.Length; j++)
+        {
+            if ( check == arr[j]) 
+            {
+                count=count+1; 
+                arr[j] = 0;
+            }
+        }
+        if (arr[i]==0) Console.Write("");
+        else
+        {
+        Console.WriteLine($"number {arr[i]} here {count} times");
+        }
+    }
+}
+
+int leng = Prompt("text a length");
+
+int [] array = GetFillFromKeyBoard(leng);
+PrintArray(array);
+
+Console.WriteLine();
+Often(array);
