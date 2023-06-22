a2d2fea7d0294d3c4f5540b6789c853eff8e15e1
Dzihiko <anna.bond1702@gmail.com>
2023-06-22 15:43:29 +0300

    рекурсия: натур числа

---
+++ task28/Program.cs
@@ -0,0 +1,20 @@
+﻿// рекурсия. вывод всех натур чисел до N
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+void NaturNum(int num, int count)
+{
+    if (count <= num)
+    {
+        Console.Write($"{count} ");
+        NaturNum(num, count+1);
+    }
+}
+int number = Prompt("text a number");
+int i = 1;
+NaturNum(number, i);
