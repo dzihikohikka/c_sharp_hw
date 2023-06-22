e123202f727d46964877f7af2dd02cf22b0eb1e6
Dzihiko <anna.bond1702@gmail.com>
2023-06-22 17:06:57 +0300

    сумма цифр числа

---
+++ task30/Program.cs
@@ -0,0 +1,21 @@
+﻿// вывод суммы цифр числа
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+int SumNum(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    
+    return (num%10 +  SumNum(num / 10));
+}
+
+int number = Prompt("text number");
+Console.WriteLine (SumNum(number));
