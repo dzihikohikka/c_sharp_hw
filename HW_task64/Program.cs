f7bbbf6f178889fc65db02816ed0477ec0f688b3
Dzihiko <anna.bond1702@gmail.com>
2023-06-22 17:29:36 +0300

    натур от числа до числа

---
+++ HW_task64/Program.cs
@@ -0,0 +1,23 @@
+﻿// вывод натур чисел от m до n
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+void NaturNum(int minNum, int num)
+{
+    if (minNum <= num)
+    {
+        Console.Write($"{minNum} ");
+        NaturNum(minNum+1, num);
+    }
+}
+
+int minNumber = Prompt("text min number");
+int number = Prompt("text max number");
+
+NaturNum(minNumber, number);
+
