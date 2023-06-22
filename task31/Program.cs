42908014521f5894905398bad11b9d5c72a1377d
Dzihiko <anna.bond1702@gmail.com>
2023-06-22 17:21:30 +0300

    степеь рекурс

---
+++ task31/Program.cs
@@ -0,0 +1,25 @@
+﻿// возведение числа в степень
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+int Step(int first, int second, int count)
+{
+    if (count == second)
+    {
+        return 1;
+    }
+
+    return (first*Step(first,second, count +1));
+}
+
+int a = Prompt("text a");
+int b = Prompt("text b");
+int k = 0;
+
+Console.WriteLine();
+Console.Write(Step(a,b,k));
