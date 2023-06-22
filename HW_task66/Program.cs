3ec4743cae8c0eff2a5a3309844ceb93fbceeaed
Dzihiko <anna.bond1702@gmail.com>
2023-06-22 17:37:15 +0300

    сумма от числ до числ

---
+++ HW_task66/Program.cs
@@ -0,0 +1,26 @@
+﻿// сумма натур от n до m
+
+// вывод натур чисел от m до n
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()!);
+    return a;
+}
+
+int NaturSum(int minNum, int num)
+{
+    if (minNum == num+1)
+    {
+        return 0;
+    }
+    
+    return (minNum+NaturSum(minNum+1, num));
+}
+
+int minNumber = Prompt("text min number");
+int number = Prompt("text max number");
+
+Console.Write(NaturSum(minNumber, number));
+
